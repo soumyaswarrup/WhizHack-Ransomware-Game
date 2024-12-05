eda5b3ec8052f074fbe1fdd3826d108a15cb8b5d
soumyaswarrup <soumyasahoo2212@gmail.com>
2024-12-05 09:16:49 +0530

    removed 5 questions

--- constants.js
+++ constants.js
@@ -49,56 +49,6 @@ export const QUESTIONS = [
     ],
     answer: 2,
   },
-  {
-    question:
-      "Stakeholders are demanding updates. How do we manage ongoing communication?",
-    options: [
-      "Provide minimal updates to avoid potential legal issues.",
-      "Establish a regular schedule of transparent, detailed updates.",
-      "Defer all communication to legal team.",
-    ],
-    answer: 1,
-  },
-  {
-    question:
-      "We've detected attempts at a second wave of attacks. What's our immediate response?",
-    options: [
-      "Implement additional security measures and monitor closely.",
-      "Shut down all systems temporarily.",
-      "Conduct another round of employee training.",
-    ],
-    answer: 0,
-  },
-  {
-    question:
-      "Our backup systems were compromised. How do we approach data recovery?",
-    options: [
-      "Attempt to recover data from potentially infected backups.",
-      "Recreate lost data manually from other sources.",
-      "Utilize offsite, uncompromised backups for recovery.",
-    ],
-    answer: 2,
-  },
-  {
-    question:
-      "We need to rebuild trust with our stakeholders. What's the best approach?",
-    options: [
-      "Offer compensation for any losses incurred.",
-      "Publish a detailed post-mortem of the incident.",
-      "Host a series of town halls and Q&A sessions with key stakeholders.",
-    ],
-    answer: 2,
-  },
-  {
-    question:
-      "As we wrap up this incident, what's our strategy moving forward?",
-    options: [
-      "Focus on damage control and public relations.",
-      "Implement a long-term cybersecurity improvement plan.",
-      "Return to business as usual with minor security adjustments.",
-    ],
-    answer: 1,
-  },
 ];
 
 export const XC_TOKEN = import.meta.env.VITE_XC_TOKEN;
